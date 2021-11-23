7cf839f9f2b3a1d96897122640fed2d564e60253
zafirskthelifehacker <zafir.heerah@spoonconsulting.com>
2021-11-23 11:22:10 +0400

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { Media } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
 import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +15,8 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SpeechRecognition],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SpeechRecognition, MediaCapture, Media, File],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/speech/speech.component.ts
+++ src/app/speech/speech.component.ts
@@ -1,5 +1,11 @@
 import { Component, NgZone, OnInit } from '@angular/core';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
 import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { Platform } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+
+const MEDIA_FILES_KEY = 'mediaFiles';
 
 @Component({
   selector: 'app-speech',
@@ -8,34 +14,86 @@ import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 })
 export class SpeechComponent implements OnInit {
 
+  mediaFiles = [];
+
+  isAudioRecording;
+  audio;
+  audioPath;
+
   listenText;
 
-  constructor(private speechRecognition: SpeechRecognition, public zone: NgZone) {
+  constructor(private speechRecognition: SpeechRecognition, private file: File, private storage: Storage,
+    private media: Media, private platform: Platform, public zone: NgZone) {
     this.speechRecognition.requestPermission().then(
       () => console.log('Granted'),
       () => console.log('Not Granted'),
     );
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.storage.get(MEDIA_FILES_KEY).then(response => {
+      this.mediaFiles = JSON.parse(response) || [];
+    });
+  }
 
-  listenToVoice() {
-    let temp;
-    this.speechRecognition.startListening()
+  async listenToVoice() {
+    this.captureAudio();
+    await this.speechRecognition.startListening()
     .subscribe(
       (matches: string[]) => {
         console.log(matches);
-        temp = matches[0];
         this.zone.run(() => {
           this.listenText = matches[0];
         });
-
       },
-      (onerror) => console.log('error:', onerror),
-      () => {
-        this.listenText = temp;
-      }
-    );
+      (onerror) => console.log('error:', onerror)
+    );;
+    this.stopCaptureAudio();
+  }
+
+  async stopListenToVoice() {
+    this.speechRecognition.stopListening();
+  }
+
+  captureAudio() {
+    try {
+       let fileName =
+          'stt_record_' +
+          new Date().getDate() + '_' +
+          new Date().getMonth() + '_' +
+          new Date().getFullYear() + '_' +
+          new Date().getHours() + '_' +
+          new Date().getMinutes() + '_' +
+          new Date().getSeconds();
+       if (this.platform.is('ios')) {
+          fileName = fileName + '.m4a';
+          this.audioPath = this.file.documentsDirectory + fileName;
+          this.audio = this.media.create(this.audioPath.replace(/file:\/\//g, ''));
+       } else if (this.platform.is('android')) {
+          fileName = fileName + '.mp3';
+          this.audioPath = this.file.externalDataDirectory + fileName;
+          this.audio = this.media.create(this.audioPath.replace(/file:\/\//g, ''));
+       }
+       this.audio.startRecord();
+       this.isAudioRecording = true;
+    } catch (error) {
+       console.log(error);
+    }
+  }
+
+  stopCaptureAudio() {
+    this.audio.stopRecord();
+    this.isAudioRecording = false;
+  }
+
+  playAudio() {
+    try {
+       this.audio = this.media.create(this.audioPath);
+       this.audio.play();
+       this.audio.setVolume(0.8);
+    } catch (error) {
+       console.log(error);
+    }
   }
 
 }
